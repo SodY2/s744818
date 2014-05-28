19ec2d9649915bdd3821a1ea0651ce2ca7c31255
Adrian <addi1986@gmail.com>
2014-05-28 15:42:21 +0200

    korrektur

---
+++ Documents/Webseiten/Ghostlab/Ghostlab_files/track.js
@@ -0,0 +1,1 @@
+__uvSessionData0({"uvts":"1EiMKFBaxcHZE55P"});
---
+++ Documents/Webseiten/PhantomLimb/PhantomLimb_files/de.js
@@ -0,0 +1,29 @@
+// de
+// German / Deutsch
+a2a_localize = {
+   Share: "Teilen",
+   Save: "Sichern",
+   Subscribe: "Abonnieren",
+   Email: "E-Mail",
+   Bookmark: "Lesezeichen",
+   ShowAll: "Alle anzeigen",
+   ShowLess: "Weniger anzeigen",
+   FindServices: "Finde Dienste",
+   FindAnyServiceToAddTo: "Finde deinen Bookmarkdienst",
+   PoweredBy: "Unterst&#252;tzt von",
+   ShareViaEmail: "Teile per E-Mail",
+   SubscribeViaEmail: "Abonniere per E-Mail",
+   BookmarkInYourBrowser: "Lesezeichen im Browser setzen",
+   BookmarkInstructions: "Benutze die Tastenkombination Strg+D, um die Seite zu speichern",
+   AddToYourFavorites: "Als Favorit setzen",
+   SendFromWebOrProgram: "Sende diese Seite von einem Mailservice oder -programm aus",
+   EmailProgram: "E-Mail-Programm"
+};
+a2a_add_services = [
+	{
+		name: 'Favoriten',
+		safe_name: 'favoriten',
+		icon: 'http://www.favoriten.de/buttons/16x16_3.gif',
+		home: 'http://www.favoriten.de/'
+	}
+];
